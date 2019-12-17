b61c3c52f00ec6af1923b2fb1a75b4575217e3aa
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2019-12-17 10:33:55 -0800

    Update dumpauthy.js

--- ios/dumpauthy.js
+++ ios/dumpauthy.js
@@ -31,10 +31,10 @@ if (ObjC.available) {
 	for (let i = 0; i < authyToken.length; i++) {
 		let token = authyToken[i];
 		if (token.$className === "AuthyToken") {
-			send(`otpauth://totp/${token["- name"]()}:${token["- name"]()}?secret=${hex_to_b32(token["- tokenData"]()["- cachedSecretKey"]().toString())}&issuer=${token["- name"]()}&digits=7`);
+			send(`otpauth://totp/${token["- name"]()}:${token["- name"]()}?secret=${hex_to_b32(token["- tokenData"]()["- cachedSecretKey"]().toString())}&issuer=${token["- name"]()}&digits=7&period=10`);
 		}
 		if (token.$className === "GoogleAuthToken") {
-			send(`otpauth://totp/${token["- accountName"]()}:${token["- originalName"]()}?secret=${token["- cachedSecretKey"]()}&issuer=${token["- originalIssuer"]()}&digits=${token["- digits"]()}`)
+			send(`otpauth://totp/${token["- accountName"]()}:${token["- originalName"]()}?secret=${token["- cachedSecretKey"]()}&issuer=${token["- originalIssuer"]()}&digits=${token["- digits"]()}&period=30`)
 		}
 	}
 }
